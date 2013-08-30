7285b6090274e4611be344700e217f1b7927f4f1
leishman <leishman3@gmail.com>
2013-08-30 00:46:10 +0000

    Added maply v0.1git add .

--- web.js
+++ web.js
@@ -1,16 +1,4 @@
-var express = require('express');
-var fs = require("fs");
-var buf = fs.readFileSync('index.html');
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-    var data = buf.toString();
-    response.send(data);
-  
-});
-
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-
+var connect = require('connect');
+connect.createServer(
+    connect.static(__dirname)
+).listen(8080);
